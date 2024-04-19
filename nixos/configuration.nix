f2b0e836406b8678ab8567d7d2bcdfde50335045
JuanBaut <deserd@protonmail.com>
2024-04-19 00:44:09 -0500

    testing with tlp

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -107,22 +107,22 @@
   services.tlp = {
     enable = true;
     settings = {
-      PCIE_ASPM_ON_AC = "default";
+      #PCIE_ASPM_ON_AC = "default";
       PCIE_ASPM_ON_BAT = "powersupersave";
 
-      RUNTIME_PM_ON_AC = "auto";
+      #RUNTIME_PM_ON_AC = "auto";
       RUNTIME_PM_ON_BAT = "auto";
 
-      PLATFORM_PROFILE_ON_AC = "balanced";
+      #PLATFORM_PROFILE_ON_AC = "balanced";
       PLATFORM_PROFILE_ON_BAT = "low-power";
 
-      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      #CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
+      #CPU_ENERGY_PERF_POLICY_ON_AC = "power";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
 
-      CPU_MAX_PERF_ON_AC = 100;
+      #CPU_MAX_PERF_ON_AC = 100;
       CPU_MAX_PERF_ON_BAT = 40;
 
       START_CHARGE_THRESH_BAT0 = 40;
