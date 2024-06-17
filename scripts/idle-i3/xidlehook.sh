db2a76b6180606f5514b9da290588037cf511812
JuanBaut <deserd@protonmail.com>
2024-06-17 12:39:11 -0500

    hyprland key repeat, and i3 scripts path change

--- scripts/idle-i3/xidlehook.sh
+++ scripts/idle-i3/xidlehook.sh
@@ -4,10 +4,10 @@ xidlehook \
     --not-when-fullscreen \
     --not-when-audio \
     --timer 60 \
-    "$HOME/.config/i3/scripts/idle/pre_idle.sh" \
-    "$HOME/.config/i3/scripts/idle/post_idle.sh" \
+    "./pre_idle.sh" \
+    "./post_idle.sh" \
     --timer 10 \
-    "$HOME/.config/i3/scripts/idle/lock_idle.sh; $HOME/.config/i3/scripts/idle/post_idle.sh" \
+    "./lock_idle.sh; ./post_idle.sh" \
     ''\
     --timer 300 \
     'systemctl suspend' \
