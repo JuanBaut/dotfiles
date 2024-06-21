dd821a56c1fbe0b2e4115cd4d53a3fb8dfe59c88
JuanBaut <deserd@protonmail.com>
2024-06-21 00:59:14 -0500

    small style changes

--- scripts/fzf/dev_search.sh
+++ scripts/fzf/dev_search.sh
@@ -9,7 +9,7 @@ directories+=(--hidden --exclude .git --exclude node_module --exclude .cache --e
 directories+=(--exact-depth 1)
 
 # Use the array to execute the fd command and pipe to fzf-tmux for fuzzy finding in a tmux pane
-selected_dir=$("${directories[@]}" | fzf-tmux --layout=reverse -h 40% )
+selected_dir=$("${directories[@]}" | fzf-tmux --layout=reverse -h 10 -w 40  )
 
 if [ -n "$selected_dir" ]; then
     cd "$selected_dir" || exit
